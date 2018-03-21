1e9885ecb99c0e6e4f06c240a087a4f428eba28f
Shikhar Bhardwaj <shikharbhardwaj68@gmail.com>
2018-03-21 20:22:45 +0530

    Initial routes and view set.

--- index.js
+++ index.js
@@ -8,22 +8,28 @@ const app = express();
 app.set('views', path.join(__dirname, 'views'));
 
 // Set external static assets folder.
-app.use('/deps', express.static(path.join(__dirname, 'node_modules', '@bower_components')));
+app.use('/deps', express.static(path.join(__dirname, 'node_modules',
+        '@bower_components')));
 
 // Set internal static assets folder.
-app.use('/assets', express.static(path.join(__dirname, 'public')));
+app.use('/', express.static(path.join(__dirname, 'public')));
 
 // Set the view engine.
 app.set('view engine', 'pug');
 
 // Home route(crime map).
 app.get('/', function(req, res) {
-  res.render('home', {title: 'Crime map'});
+  res.render('home', {title: 'Satark'});
 });
 
 // Analytics route.
-app.get('/analysis', function(req, res) {
-  res.render('analysis', {title: 'Crime data analysis'});
+app.get('/analytics', function(req, res) {
+  res.render('analysis', {title: 'Satark | Crime data analysis'});
+});
+
+// Admin route.
+app.get('/login', function(req, res) {
+  res.render('login', {title: 'Satark | Administrator login'});
 });
 
 // Start server.
