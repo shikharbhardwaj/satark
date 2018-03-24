151596a75ee2da3d53c5a497866d3dfa214799b6
shikharbhardwaj <shikharbhardwaj68@gmail.com>
2018-03-24 18:13:43 +0530

    Add required dependencies for database interaction.

--- knexfile.js
+++ knexfile.js
@@ -1,24 +1,24 @@
-const databaseName = 'passport_local_knex';
+// const databaseName = 'passport_local_knex';
 
 module.exports = {
   development: {
     client: 'postgresql',
     connection: 'postgres://localhost:5432/satark_db',
     migrations: {
-      directory: __dirname + '/src/server/db/migrations'
+      directory: __dirname + '/src/server/db/migrations',
     },
     seeds: {
-      directory: __dirname + '/src/server/db/seeds'
-    }
+      directory: __dirname + '/src/server/db/seeds',
+    },
   },
   test: {
     client: 'postgresql',
     connection: 'postgres://localhost:5432/satark_db_testing',
     migrations: {
-      directory: __dirname + '/src/server/db/migrations'
+      directory: __dirname + '/src/server/db/migrations',
     },
     seeds: {
-      directory: __dirname + '/src/server/db/seeds'
-    }
-  }
+      directory: __dirname + '/src/server/db/seeds',
+    },
+  },
 };
