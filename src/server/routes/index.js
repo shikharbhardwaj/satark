003a3b12634caa0f10b2b954599f2680aa82165c
shikharbhardwaj <shikharbhardwaj68@gmail.com>
2018-03-31 12:26:18 +0530

    Complete front-end integration.

--- src/server/routes/index.js
+++ src/server/routes/index.js
@@ -4,6 +4,10 @@ const router = express.Router();
 const indexController = require('../controllers/index');
 
 router.get('/', function (req, res, next) {
+  res.render('index', {title: 'Satark'});
+});
+
+router.get('/login', function (req, res, next) {
   res.render('login', {title: 'Administrator login | Satark'});
 });
 
